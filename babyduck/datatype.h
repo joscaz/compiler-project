ad90e3f6ed9103ca2160aea41d4fb47a424321f4
joscaz <joscazertuche@gmail.com>
2025-05-07 18:19:01 -0600

    fix: datatype header created since it needs to be created before semantic analysis

---
+++ babyduck/datatype.h
@@ -0,0 +1,12 @@
+#ifndef DATATYPE_H
+#define DATATYPE_H
+
+// Definition for Babyduck types
+typedef enum {
+    TYPE_ERROR = -1, // for invalid operations
+    TYPE_INT = 0,
+    TYPE_FLOAT = 1,
+    TYPE_BOOL = 2
+} DataType;
+
+#endif // DATATYPE_H
