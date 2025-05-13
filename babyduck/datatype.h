b499feabe2deea22a911c3e0c6de8a55cb5a9664
joscaz <joscazertuche@gmail.com>
2025-05-13 16:28:43 -0600

    Added string datatype

--- babyduck/datatype.h
+++ babyduck/datatype.h
@@ -6,7 +6,8 @@ typedef enum {
     TYPE_ERROR = -1, // for invalid operations
     TYPE_INT = 0,
     TYPE_FLOAT = 1,
-    TYPE_BOOL = 2
+    TYPE_BOOL = 2,
+    TYPE_STRING = 3
 } DataType;
 
 #endif // DATATYPE_H
